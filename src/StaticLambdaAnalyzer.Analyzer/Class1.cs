bf5b57645e309589c077e5c27e7fb8fd4f849151
gurustron <gurustronpublic@gmail.com>
2026-05-30 10:46:32 +0300

    initial

---
+++ src/StaticLambdaAnalyzer.Analyzer/Class1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace StaticLambdaAnalyzer.Analyzer
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ src/StaticLambdaAnalyzer.TestApp/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Hello, World!");
+
+int fortyTwo = 42;
+assertStatic(true, staticFunction); //works (trivial)
+assertStatic(false, nonStaticFunction); //works (trivial)
+assertStatic(false, () => fortyTwo); //works (happy path)
+assertStatic(true, () => 42); //fails
+assertStatic(true, static () => 42); //fails too
+return;
+
+static int staticFunction() => 42;
+int nonStaticFunction() => fortyTwo;
+
+static void assertStatic<T>(bool shouldBeStatic, System.Func<T> action)
+{
+    Console.WriteLine($"Assert: {IsStatic(action) == shouldBeStatic}");
+}
+
+static bool IsStatic(System.Delegate @delegate)
+{
+    bool hasTarget = @delegate.Target != null;
+    Console.WriteLine(@delegate.Method.IsStatic == !hasTarget);
+    return !hasTarget;
+}
